299c83fd06a636bec64143b41359dfa27ab6f360
Italo <italofsilva583@gmail.com>
2023-07-30 00:13:23 -0300

    Alteração nos tamanhos

--- src/components/DivMaster.jsx
+++ src/components/DivMaster.jsx
@@ -47,86 +47,25 @@ export default function DivMaster() {
     }
 
     return(
-        // <div className='DivMaster '>
-        //     <div>
-        //         <h2 className='title'>Calcular IMC</h2>
-                
-        //         <div className='DivPesoAltura'>
-        //             <h3>Peso: </h3>
-        //             <input type="number" className='Input ' placeholder='Quilos' value={peso} 
-        //             onChange={event => {setPeso(event.target.value);}}/>
-                    
-        //             <h3>Altura: </h3>
-        //             <input type="number" className='Input ' placeholder='Metros' value={altura} 
-        //             onChange={event => {setAltura(event.target.value);}} />
-        //         </div>
-
-        //         <div className='DivBotao'>
-        //             <div>
-        //                 <button type="button" className='Button btn ' id='calcular' onClick={calcularImc}><strong>Calular</strong></button>
-        //             </div>
-        //             <div>
-        //                 <button type="button" className='Button btn ' id='limpar' onClick={limpar}><strong>Limpar</strong></button>
-        //             </div>
-        //         </div>
-
-        //         <div className='DivResultado '>
-        //             <h3>SEU IMC:</h3>
-        //             <input type='number' className='Input' placeholder='Resultado' id='resultado' value={imc} readOnly/>
-        //         </div>
-
-        //         <div className='DivTabela'>
-                    
-                //     <table>
-                //     <h3 className=''>Indices: </h3>			
-                //         <tr>    
-				// 			<td><h3>IMC</h3></td>
-				// 			<td><h3>Classificação</h3></td>
-				// 		</tr>
-				// 		<tr id='magreza'  >
-				// 			<td>Menor que 18,5: </td>
-				// 			<td>Magreza</td>
-				// 		</tr>
-				// 		<tr id='normal' >
-				// 			<td>Entre 18,5 e 24,9: </td>
-				// 			<td>Normal</td>
-				// 		</tr>
-				// 		<tr id='sobrepeso' >
-				// 			<td>Entre 25,0 e 29,9: </td>
-				// 			<td>Sobrepeso</td>
-				// 		</tr>
-				// 		<tr id='obesidade' >
-				// 			<td>Entre 30,0 e 39,9: </td>
-				// 			<td>Obesidade</td>
-				// 		</tr>
-				// 		<tr id='grave' >
-				// 			<td>Maior que 40,0: </td>
-				// 			<td>Obesidade Grave</td>
-				// 		</tr>
-				
-				// </table>
-        //         </div>
-        //     </div>
-        // </div>
         
-        <div className='container DivMaster'>
-            <div class="row   ">
-                <h2 className='title'>Calcular IMC</h2>
+        <div className='container DivMaster w-50'>
+            <h2 className='title'>Calcular IMC</h2>
+            <div class="row ">  
                 
-                <div class="col DivPesoAltura mb-3">
-                <h3>Peso: </h3>
-                <input type="number" className='Input ' placeholder='Quilos' value={peso} 
-                onChange={event => {setPeso(event.target.value);}}/>
-                        
-                <h3>Altura: </h3>
-                <input type="number" className='Input ' placeholder='Metros' value={altura} 
-                onChange={event => {setAltura(event.target.value);}} />
-
+                <div class="col DivPesoAltura p-5">
+                    <h3>Peso: </h3>
+                    <input type="number" className='Input form-control' placeholder='Quilos' value={peso} 
+                    onChange={event => {setPeso(event.target.value);}}/>
+                            
+                    <h3>Altura: </h3>
+                    <input type="number" className='Input form-control' placeholder='Metros' value={altura} 
+                    onChange={event => {setAltura(event.target.value);}} />
                 </div>
-                <div class="col DivBotao mb-3">
+
+                <div class="col DivBotao p-1">
+                    <br />
                     <div>
                         <button type="button form-control" className='Button btn ' id='calcular' onClick={calcularImc}><strong>Calular</strong></button>
-                        
                     </div>
                     <div>
                         <button type="button form-control" className='Button btn ' id='limpar' onClick={limpar}><strong>Limpar</strong></button>
@@ -134,22 +73,22 @@ export default function DivMaster() {
 
                 </div>
                     
-
-
-                <div class="col DivResultado mb-3">
+                <div class="col DivResultado p-1">
+                    <br /><br />
                     <h3>SEU IMC:</h3>
                     <input type='number' className='Input' placeholder='Resultado' id='resultado' value={imc} readOnly/>
 
-
                 </div>
-                <div class="col DivTabela mb-3">
+                
+            </div>
+            <div class="col DivTabela p-1">
                         <table className=''>
                         <h3 className=''>Indices: </h3>			
                             <tr>    
-                                <td><h3>IMC</h3></td>
-                                <td><h3>Classificação</h3></td>
+                                <th><h3>IMC</h3></th>
+                                <th><h3>Classificação</h3></th>
                             </tr>
-                            <tr id='magreza'  >
+                            <tr className='' id='magreza'  >
                                 <td>Menor que 18,5: </td>
                                 <td>Magreza</td>
                             </tr>
@@ -172,10 +111,7 @@ export default function DivMaster() {
                     
                     </table>
 
-
                 </div>
-                
-            </div>
         </div>
         
 
